021a94873041c437fd07b873cff91fdd6bbf30a4
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-06-21 19:44:14 +0100

    creating Service Worker

---
+++ public/serviceworker.js
@@ -0,0 +1,12 @@
+//Service Workers js file that run's all the time
+//keep running even after closing the page
+
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
+
+const self = this; // represent the sw
+//Install Sw
+
+//Listen for requests
+
+//Activate Sw
